1e91a4c07a8815a0c17d88b3e01b4f8d828c358d
Faisal Abuzaid <faisalabuzaid@outlook.com>
2021-02-22 15:30:54 +0200

    editapp

--- js/app.js
+++ js/app.js
@@ -23,38 +23,53 @@ const names = [
         'chair.jpg'
       ];
 
-let maxReach=0;
+
+      let repeatCheck = [];
+
+      let maxReach=0;
   
+
+      let buttonPressed=false;
+
+    
   const firstImage = document.getElementById('first-image');
   const secondImage = document.getElementById('second-image');
   const thirdImage = document.getElementById('third-image');
-  console.log(firstImage);
   const imagesSection = document.getElementById('img-section');
-  // const goats = [];
+  
   function Buss(name) {
-    this.name = name;
+    this.name = name.split('.')[0];
     this.path = `./img/${name}`;
     this.votes = 0;
     this.views = 0;
-    //goats.push(this);
     Buss.all.push(this);
   }
-  Buss.all = [];
-  for (let i = 0; i < names.length; i++) {
+
+Buss.all=[];
+
+for (let i = 0; i < names.length; i++) {
     new Buss(names[i]);
   }
-  console.table(Buss.all);
-  const noRepeatArray = [];
+
+
+  
+
   function render() {
     let firstImgArr = Buss.all[randomNumber(0, Buss.all.length - 1)];
     let secondImgArr = Buss.all[randomNumber(0, Buss.all.length - 1)];
     let thirdImgArr = Buss.all[randomNumber(0, Buss.all.length - 1)];
     console.table(firstImgArr);
-    while (firstImgArr.name===secondImgArr.name || secondImgArr.name===thirdImgArr.name || firstImgArr.name===thirdImgArr.name) {
+    while (firstImgArr.name===secondImgArr.name || secondImgArr.name===thirdImgArr.name || firstImgArr.name===thirdImgArr.name || repeatCheck.includes(firstImgArr) || repeatCheck.includes(secondImgArr) || repeatCheck.includes(thirdImgArr)) {
       firstImgArr = Buss.all[randomNumber(0, Buss.all.length - 1)];;
       secondImgArr = Buss.all[randomNumber(0, Buss.all.length - 1)];
       thirdImgArr = Buss.all[randomNumber(0, Buss.all.length - 1)];
     }
+
+    repeatCheck = [];
+    repeatCheck.push(firstImgArr);
+    repeatCheck.push(secondImgArr);
+    repeatCheck.push(thirdImgArr);
+
     console.log('first', firstImgArr.path);
     firstImage.src = firstImgArr.path;
     firstImage.title = firstImgArr.name;
@@ -73,41 +88,84 @@ let maxReach=0;
     thirdImage.alt = thirdImgArr.name;
     thirdImgArr.views++;
 
-    if(maxReach==25) {
+    if(maxReach==5 && document.getElementById("button").disabled == false) {
         let printResult=document.getElementById("mylist");
+            console.log(printResult);
+            for (let i = 0; i < Buss.all.length; i++) {
+                let print = document.createElement('li');
+                printResult.appendChild(print);
+               print.textContent = `${(Buss.all[i].name).toUpperCase()} had ${Buss.all[i].votes} Votes and was seen ${Buss.all[i].views} Times.`;
+               console.log(print);}
+               document.getElementById('button').disabled = true;
+    
+      
+      imagesSection.removeEventListener('click', handleClick);
 
-        for (let i = 0; i < Buss.all.length; i++) {
-            const print = document.createElement('li');
-            printResult.appendChild(print);
-           print = `${Buss.all[i].name} votes are ${Buss.all[i].votes} and Views are ${Buss.all[i].views}`
-            
         }
     }
 
-  }
   imagesSection.addEventListener('click', handleClick);
   
   function handleClick(event) {
     console.log('Target', event.target.id);
     
-        if (event.target.id !== 'img-section' && maxReach!=25) {
+        if (event.target.id !== 'img-section') {
             for (let i = 0; i < Buss.all.length; i++) {
               if (Buss.all[i].name === event.target.title) {
                 Buss.all[i].votes++;
                 maxReach++;
-                // Buss.all[i].votes = Buss.all[i].votes + 1
               }
             }
-            console.log(Buss.all);
             render();
           }
      
     }
     
+   
 
-  //helper functions
   function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
   
+  function chart(){
+    const allVotes = [];
+    const allVeiws = [];
+    const allNames = [];
+
+    var ctx = document.getElementById('my-chart').getContext('2d');
+    for (let i = 0; i < Buss.all.length; i++) {
+      allVotes.push(Buss.all[i].votes);
+      allVeiws.push(Buss.all[i].views);
+      allNames.push(Buss.all[i].name);
+      console.log(allVeiws);
+      console.log(allVotes);
+    }
+    var chart = new Chart(ctx, {
+      // The type of chart we want to create
+      type: 'bar',
+
+      // The data for our dataset
+      data: {
+        labels: allNames,
+        datasets: [{
+          label: 'Votes',
+          backgroundColor: 'rgb(25, 99, 132)',
+          borderColor: 'rgb(25, 199, 132)',
+          data: allVotes
+        },
+        {
+          label: 'Veiws',
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: allVeiws
+        }
+
+        ]
+      },
+
+      // Configuration options go here
+      options: {}
+    });
+  }
+  chart();
   render();
